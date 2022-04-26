8d1d245d4b53fbecade81aeb791f86c7c5bd911e
victorudenna@gmail.com <victorudenna@gmail.com>
2022-04-26 09:15:29 +0100

    Added all files

---
+++ index.js
@@ -0,0 +1,130 @@
+
+let names;
+names = "victor";
+
+console.log(names);
+
+
+let a;
+a = 10;
+
+let b;
+b = 5;
+
+let c;
+c = 7;
+
+let result;
+
+result = a * b * c;
+console.log(result);
+
+
+// Assigment 1
+
+let phone = {
+    brand: "Apple",
+    model: "iphone 12",
+    color: "white",
+    Ram: "4GB",
+    year: 2021
+};
+
+console.log(phone); 
+
+
+let pc = {
+    brand: "Dell",
+    model: "E6540",
+    color: "space grey",
+    ram: "8GB",
+    year: 2013
+};
+console.log(pc);
+
+
+const person = {
+    firstName: "victor",
+    lastName: "Okwuosa",
+    age: 18,
+    eyeColor: "dark browm",
+    Height: "179cm"
+};
+
+console.log(person);
+
+
+let car = {
+    brand: "Range rover",
+    model:"vogue",
+    capacity:"5 seat",
+    year: 2021,
+    price: "$100,000"
+
+};
+
+console.log(car);
+
+let device = {
+    brand: "Samsung",
+    model: "S22",
+    color: "white",
+    Ram: "8GB",
+    year: 2022
+};
+
+console.log(device);
+
+
+
+// Assingment2
+
+
+
+
+
+
+let Toyota = {
+    trim: "trd",
+    model: "Camry",
+    seat: 5,
+    fuel: "petrol",
+    year: 2022
+}; 
+
+let Lamborghini = {
+    trim: "Evo",
+    model: "huracan",
+    seat: 3,
+    fuel: "petrol",
+    year: 2019
+}; 
+
+let Mercedes = {
+    trim: "4matic+",
+    model: "gle",
+    seat: "7",
+    fuel: "petrol",
+    year: 2020
+};
+
+let Ford = {
+    trim: "GT500",
+    model: "mustang",
+    seat: 4,
+    fuel: "petrol",
+    year: 2018
+};
+
+
+let Honda = {
+    trim: "LX",
+    model: "Civic",
+    seat: 5,
+    fuel: "petrol",
+    year: 2017
+}; 
+
+let objectinArray = [Toyota, Lamborghini, Mercedes, Ford, Honda]
+
+console.log(objectinArray)
